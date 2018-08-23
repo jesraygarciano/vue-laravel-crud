4f4309972c748bc21e4fe361eb0301275f37d729
Jesray Garciano <jesray.garciano@gmail.com>
2018-08-23 13:55:14 +0800

    Added Create, index and update method in Cruds Controller

--- app/Http/Controllers/CrudsController.php
+++ app/Http/Controllers/CrudsController.php
@@ -10,5 +10,25 @@ use Faker\Generator;
 class CrudsController extends Controller
 {
     //
+    public function create(Generator $faker){
+        $crud = new Crud();
+        $crud->name = $faker->lexify('????????');
+        $crud->color = $faker->boolean ? 'red' : 'green';
+        $crud->save();
+
+        return response($crud->jsonSerialize(), Response::HTTP_CREATED);
+    }
+
+    // Return full set of crud
+    public function index(){
+        return response(Crud::all()->jsonSerialize(), Response::HTTP_OK);
+    }
     
+    // Change color of the Crud
+    public function update(Request $request, $id){
+        $crud = Crud::findOrFail($id);
+        $crud->color = $request->color;
+        $crud->save();
+    }
+
 }
