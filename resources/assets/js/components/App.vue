3b7bc961ab05435d4a50209d40ef776717a0d3f1
Jesray Garciano <jesray.garciano@gmail.com>
2018-08-23 15:05:14 +0800

    Added App vue in Components vue directory

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,3 +1,55 @@
 <template>
-    
+    <div id="app">
+        <div class="heading">
+            <h1>Cruds</h1>
+        </div>
+
+        <crud-component
+        v-for= "crud in cruds"
+        v-bind= "crud"
+        :key="crud.id"
+        @update="update"
+        @delete="del"
+        >
+        </crud-component>
+        <div>
+            <button @click="create()">Add</button>
+        </div>
+    </div>
 </template>
+<script>
+    function Crud({id, color}) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+    }
+
+    import CrudComponent from './CrudComponent.vue';
+
+    export default{
+        data() {
+            return {
+                cruds: []
+            }
+        },
+
+        methods: {
+            create() {
+                // Todo
+            },
+            read() {
+                // Todo
+            },
+            update(id, color) {
+                // To do
+            },
+            del(id) {
+                // Todo
+            }
+        },
+
+        components: {
+            CrudComponent
+        }
+    }
+</script>
