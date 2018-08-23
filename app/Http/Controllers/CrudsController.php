85de4606ee3b8026a1f4444c63cf028a426bccbd
Jesray Garciano <jesray.garciano@gmail.com>
2018-08-23 13:46:55 +0800

    Added Cruds Controller

---
+++ app/Http/Controllers/CrudsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Crud;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Faker\Generator;
+
+class CrudsController extends Controller
+{
+    //
+    
+}
